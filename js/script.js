368e43b2252c946c7e9e22fd3641e7c38c8b53c5
Massa Adonaï <167198345+adonaiGmassa@users.noreply.github.com>
2025-01-08 21:51:16 +0100

    js function ferme

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ function hamburg(){
     const NAVBAR = document.querySelector(".Menu-deroulant");
     NAVBAR.Style.transfom = "translateY(0px)"
 }
+function ferme(){
+    const NAVBAR = document.querySelector(".Menu-deroulant");
+    NAVBAR.Style.transfom = "translateY(-500px)"
+}
