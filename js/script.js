6c84b562f5a09ef5510778585e894eb54af1b48e
Massa Adonaï <167198345+adonaiGmassa@users.noreply.github.com>
2025-01-08 22:09:28 +0100

    js function typeWriter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 //By Radcircles
+const txtsTypewrite=[]
+const txtElement=document.querySelector(".typewriter-text")
+let vitesse= 100;
+let txtIndex = 0
+let indexLettre = 0
 
 function hamburg(){
-    const NAVBAR = document.querySelector(".Menu-deroulant");
-    NAVBAR.Style.transfom = "translateY(0px)"
+    const NAVBAR = document.querySelector(".Menu-deroulant")
+    NAVBAR.style.transform = "translateY(0px)";  
 }
+
 function ferme(){
-    const NAVBAR = document.querySelector(".Menu-deroulant");
-    NAVBAR.Style.transfom = "translateY(-500px)"
+    const NAVBAR = document.querySelector(".Menu-deroulant")
+    NAVBAR.style.transform = "translateY(-500px)"
 }
+function typeWriter(){
+    if(indexLettre<txt[txtIndex].length){
+        txtElement.innerHTML += txt[txtIndex].charAt(indexLettre)
+        indexLettre++
+        setTimeout(typeWriter,vitesse)
+    }
+    else{
+        setTimeout(eraseText,1000)
+    }
+}
+function eraseText(){}
