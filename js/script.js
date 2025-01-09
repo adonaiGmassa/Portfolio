f7711a1f5744a6c6d590a75a5c21867dd7efc508
Massa Adonaï <167198345+adonaiGmassa@users.noreply.github.com>
2025-01-09 21:54:51 +0100

    js function eraseText #2

--- js/script.js
+++ js/script.js
@@ -15,24 +15,27 @@ function ferme(){
     NAVBAR.style.transform = "translateY(-500px)"
 }
 function typeWriter(){
-    if(indexLettre<txt[txtIndex].length){
-        txtElement.innerHTML += txt[txtIndex].charAt(indexLettre)
+    if(indexLettre < txtsTypewrite[txtIndex].length){
+        txtElement.innerHTML += txtsTypewrite[txtIndex].charAt(indexLettre)
         indexLettre++
         setTimeout(typeWriter,vitesse)
     }
     else{
-        setTimeout(eraseText,1000)
+        setTimeout(eraseText,1000)// Attente de 1 seconde avant de commencer à effacer
     }
 }
-function eraseText(){
-    if(txtElement.innerHTML.length){
-        txtElement.innerHTML = txtElement.innerHTML.slice
-        indexLettre++
-        setTimeout(eraseText,50)
-    }
-    else{
-        txtIndex = (txtIndex + 1) % txtsTypewrite.length
-        indexLettre = 0
-        setTimeout(typeWriter,500)
+
+// Fonction pour effacer le texte
+function eraseText() {
+    if (txtElement.innerHTML.length) {
+        // Efface une lettre à la fois
+        txtElement.innerHTML = txtElement.innerHTML.slice(0, -1); 
+        indexLettre--; // Réduire l'index de la lettre
+        setTimeout(eraseText, 50); // Vitesse d'effacement
+    } else {
+        // Passer au texte suivant
+        txtIndex = (txtIndex + 1) % txtsTypewrite.length; // Tourner entre les textes
+        indexLettre = 0; // Réinitialiser l'index des lettres
+        setTimeout(typeWriter, 500); // Attendre 0.5 seconde avant de commencer à écrire le prochain texte
     }
 }
